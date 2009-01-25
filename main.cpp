d9fff7d1b9901398b82a9c2e3ba10075fd3a7f71
Jorge Villatoro <jorge@Jorge-Villatoros-MacBook.local>
2009-01-24 21:59:59 -0600

    Added filename argument

--- main.cpp
+++ main.cpp
@@ -20,20 +20,20 @@ int main(int argc, char **argv)
 		
 	Tone *tone;
 	
-	if(args.size() == 5) {
-		tone = new Tone(atoi(args[3].c_str()), atoi(args[4].c_str()));
+	if(args.size() == 6) {
+		tone = new Tone(atoi(args[4].c_str()), atoi(args[5].c_str()));
 	}
-	else if(args.size() == 4) {
-		tone = new Tone(atoi(args[3].c_str()));
+	else if(args.size() == 5) {
+		tone = new Tone(atoi(args[4].c_str()));
 	}
 	else
 		tone = new Tone();
 
-	if(args.size() >= 3 && tone->writeFile("test.wav", atof(args[1].c_str()), atof(args[2].c_str()))) {
+	if(args.size() >= 4 && tone->writeFile(args[1].c_str(), atof(args[2].c_str()), atof(args[3].c_str()))) {
 		cout << "Successfully created wav file" << endl;
 	}
 	else
-		cout << "Usage: " << args[0] << " pitch duration <sample rate> <bits per sample>" << endl;
+		cout << "Usage: " << args[0] << " filename pitch duration <sample rate> <bits per sample>" << endl;
 		
 	return 0;
 }
